fcf1faf4d51b26dd6c602900aa6b21273e4d535e
lukanas17 <120675562+lukanas17@users.noreply.github.com>
2023-09-06 01:19:02 +0300

    Atnaujintas dizainas ir funkcionalumas

--- DietaLangas.py
+++ DietaLangas.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import sqlite3
-
+import tkinter.font as tkFont
 
 def duomenys():
     conn = sqlite3.connect('Receptai.db')
@@ -10,19 +10,15 @@ def duomenys():
     conn.close()
     return options
 
-
 def receptai(recepto_pav):
     conn = sqlite3.connect('Receptai.db')
     cursor = conn.cursor()
-    cursor.execute('SELECT r.pavadinimas, r.paruosimo_laikas, r.porcijos, r.ingredientai, r.instrukcija, r.nuotrauka '
-                   'FROM receptai AS r '
-                   'JOIN receptai_dieta AS rd ON r.id = rd.receptas_id '
-                   'JOIN dietos AS d ON rd.dieta_id = d.id '
-                   'WHERE d.pavadinimas = ?', (recepto_pav,))
-    recepto_d = cursor.fetchall()
+    cursor.execute('SELECT pavadinimas, paruosimo_laikas, porcijos, ingredientai, instrukcija, nuotrauka '
+                   'FROM receptai '
+                   'WHERE pavadinimas = ?', (recepto_pav,))
+    recepto_det = cursor.fetchone()
     conn.close()
-    return recepto_d
-
+    return recepto_det
 
 def atnaujinti(recipes):
     listas.delete(0, tk.END)
@@ -30,9 +26,31 @@ def atnaujinti(recipes):
         recepto_pav = receptas[0]
         listas.insert(tk.END, f"Pavadinimas: {recepto_pav}, Paruošimo laikas: {receptas[1]} min")
 
+def parodyti_recepta(event):
+    selected_index = listas.curselection()
+    if selected_index:
+        selected_recipe = recipes[selected_index[0]]
+        pavadinimas, paruosimo_laikas, porcijos, ingredientai, instrukcija, nuotrauka = selected_recipe
+
+        isokantis = tk.Toplevel(langas)
+        isokantis.title("Recepto Informacija")
+        isokantis.geometry("1200x600")
+        fontas = tkFont.Font(size=12)
+        text_widget = tk.Text(isokantis, wrap=tk.WORD, font=fontas)
+        text_widget.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        details_text = f"Pavadinimas: {pavadinimas}\nParuošimo laikas: {paruosimo_laikas} min\n" \
+                       f"Porcijos dydis: {porcijos}\nIngredientai:\n {ingredientai}\nInstrukcija:\n {instrukcija}" \
+                       f"\nNuotrauka: {nuotrauka}"
+        text_widget.insert(tk.END, details_text)
+        text_widget.tag_configure("bold", font=(fontas.actual("family"), fontas.actual("size"), "bold"))
+        text_widget.tag_add("bold", "1.0", "1.13")
+        text_widget.tag_add("bold", "2.0", "2.17")
+        text_widget.tag_add("bold", "3.0", "3.15")
+        text_widget.tag_add("bold", "4.0", "4.16")
+        text_widget.tag_add("bold", "15.0", "15.15")
 
 def pasirinkti():
-    global listas
     pasirinkta_dieta = [options[i][0] for i, var in enumerate(option_vars) if var.get()]
     if not pasirinkta_dieta:
         pasirinktas_t.set("Pasirinkite bent vieną")
@@ -41,7 +59,7 @@ def pasirinkti():
         cursor = conn.cursor()
 
         query = """
-        SELECT r.pavadinimas, r.paruosimo_laikas
+        SELECT r.pavadinimas, r.paruosimo_laikas, r.porcijos, r.ingredientai, r.instrukcija, r.nuotrauka
         FROM receptai AS r
         WHERE r.id NOT IN (
             SELECT rd.receptas_id
@@ -55,26 +73,41 @@ def pasirinkti():
         recipes = cursor.fetchall()
         conn.close()
 
-        receptu_langas = tk.Toplevel(langas)
-        receptu_langas.title("Pasirinkti patiekalai")
-        receptu_langas.geometry("800x400")
+        atnaujinti(recipes)
 
-        listas = tk.Listbox(receptu_langas, width=80, height=10, font=("Arial", 14))
-        listas.pack()
+def alergijos():
+    global recipes
+    conn = sqlite3.connect('Receptai.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT pavadinimas FROM alergijos')
+    options = cursor.fetchall()
+    conn.close()
+    return options
 
-        if not recipes:
-            listas.destroy()
-            receptu_langas.geometry("370x60")
-            nera = tk.Label(receptu_langas, text="Nėra receptų tokiam pasirinkimui!",
-                            font=("Arial", 12, "bold"))
-            uzdaryti = tk.Button(receptu_langas, text="Close", command=receptu_langas.destroy)
-            nera.pack()
-            uzdaryti.pack()
+def pasirinkti_alergijas():
+    pasirinktos_alergijos = [options[i][0] for i, var in enumerate(option_vars_alergijos) if var.get()]
+    if not pasirinktos_alergijos:
+        pasirinktas_t_alergijos.set("Pasirinkite bent vieną alergiją")
+    else:
+        conn = sqlite3.connect('Receptai.db')
+        cursor = conn.cursor()
 
-        else:
-            atnaujinti(recipes)
+        query = """
+        SELECT r.pavadinimas, r.paruosimo_laikas, r.porcijos, r.ingredientai, r.instrukcija, r.nuotrauka
+        FROM receptai AS r
+        WHERE r.id NOT IN (
+            SELECT ra.receptas_id
+            FROM receptai_alergijos AS ra
+            JOIN alergijos AS a ON ra.alergija_id = a.id
+            WHERE a.pavadinimas IN ({})
+        )
+        """.format(', '.join(['?'] * len(pasirinktos_alergijos)))
 
+        cursor.execute(query, pasirinktos_alergijos)
+        recipes = cursor.fetchall()
+        conn.close()
 
+        atnaujinti(recipes)
 
 langas = tk.Tk()
 langas.title("Pasirinkite patiekalą")
@@ -88,12 +121,41 @@ for i, option_name in enumerate(options):
     checkbox.pack(anchor='w')
     checkboxes.append(checkbox)
 
-mygtukas = tk.Button(langas, text="Pasirinkti", command=pasirinkti)
+mygtukas = tk.Button(langas, text="Pasirinkti", command=pasirinkti, font=("Ariel", 11))
 mygtukas.pack()
 
 pasirinktas_t = tk.StringVar()
 pasirinktas = tk.Label(langas, textvariable=pasirinktas_t)
 pasirinktas.pack()
 
+listas = tk.Listbox(langas, width=80, height=10, font=("Arial", 14))
+listas.pack(side=tk.LEFT)
+
+# Create a scrollbar
+scrollbar = tk.Scrollbar(langas, orient=tk.VERTICAL, command=listas.yview)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+# Configure the listbox to use the scrollbar
+listas.config(yscrollcommand=scrollbar.set)
+
+# Bind the double click event to the list to display recipe details
+listas.bind('<Double-Button-1>', parodyti_recepta)
+
+# Checkboxes and button for allergies
+options_alergijos = alergijos()
+option_vars_alergijos = [tk.BooleanVar() for _ in options_alergijos]
+
+checkboxes_alergijos = []
+for i, option_name in enumerate(options_alergijos):
+    checkbox = tk.Checkbutton(langas, text=option_name[0], variable=option_vars_alergijos[i])
+    checkbox.pack(anchor='w')
+    checkboxes_alergijos.append(checkbox)
+
+mygtukas_alergijos = tk.Button(langas, text="Pasirinkti su alergijomis", command=pasirinkti_alergijas, font=("Ariel", 11))
+mygtukas_alergijos.pack()
+
+pasirinktas_t_alergijos = tk.StringVar()
+pasirinktas_alergijos = tk.Label(langas, textvariable=pasirinktas_t_alergijos)
+pasirinktas_alergijos.pack()
 
 langas.mainloop()
--- IngLangas.py
+++ IngLangas.py
@@ -26,19 +26,20 @@ def atnaujinti(recipes):
     reciptu_list.delete(0, tk.END)
     for receptas in recipes:
         recepto_pav = receptas[0]
-        reciptu_list.insert(tk.END, f"Pavadinimas: {recepto_pav}, Paruošimo laikas: {receptas[1]} min")
+        reciptu_list.insert(tk.END, f"{recepto_pav} - {receptas[1]}min")
         reciptu_list.bind('<Double-Button-1>', lambda event, recepto_pav=recepto_pav: parodyti_recepta(recepto_pav))
 
 def parodyti_recepta(recepto_pav):
-    pasirinkti = receptai(recepto_pav)
-    if pasirinkti:
-        pavadinimas, paruosimo_laikas, porcijos, ingredientai, instrukcija, nuotrauka = pasirinkti
+    selected_index = reciptu_list.curselection()
+    if selected_index:
+        selected_recipe = recipes[selected_index[0]]
+        pavadinimas, paruosimo_laikas, porcijos, ingredientai, instrukcija, nuotrauka = selected_recipe
 
-        issokantis = tk.Toplevel(langas)
-        issokantis.title("Recepto Informacija")
-        issokantis.geometry("800x600")
+        isokantis = tk.Toplevel(langas)
+        isokantis.title("Recepto Informacija")
+        isokantis.geometry("1200x600")
         fontas = tkFont.Font(size=12)
-        text_widget = tk.Text(issokantis, wrap=tk.WORD, font=fontas)
+        text_widget = tk.Text(isokantis, wrap=tk.WORD, font=fontas)
         text_widget.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
         details_text = f"Pavadinimas: {pavadinimas}\nParuošimo laikas: {paruosimo_laikas} min\n" \
@@ -51,23 +52,22 @@ def parodyti_recepta(recepto_pav):
         text_widget.tag_add("bold", "3.0", "3.15")
         text_widget.tag_add("bold", "4.0", "4.16")
         text_widget.tag_add("bold", "15.0", "15.15")
-
-        # Create a vertical scrollbar and attach it to the Text widget
-        scrollbar = tk.Scrollbar(issokantis, command=text_widget.yview)
+        scrollbar = tk.Scrollbar(isokantis, command=text_widget.yview)
         scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
         text_widget.config(yscrollcommand=scrollbar.set)
 
-def patvirtinti():
-    global reciptu_list  # Use the global variable
+def parodyti():
+    global recipes
     pasirinkti_ingredientai = [options[i][0] for i, var in enumerate(option_vars) if var.get()]
     if not pasirinkti_ingredientai:
         selected_text.set("Pasirinkti bent vieną ingredientą")
     else:
+        selected_text.set("")
         conn = sqlite3.connect('Receptai.db')
         cursor = conn.cursor()
 
         query = """
-        SELECT r.pavadinimas, r.paruosimo_laikas
+        SELECT r.pavadinimas, r.paruosimo_laikas, r.porcijos, r.ingredientai, r.instrukcija, r.nuotrauka
         FROM receptai AS r
         JOIN receptai_ingredientai AS ri ON r.id = ri.receptas_id
         JOIN ingredientai AS i ON ri.ingredientas_id = i.id
@@ -78,51 +78,45 @@ def patvirtinti():
 
         cursor.execute(query, pasirinkti_ingredientai + [len(pasirinkti_ingredientai)])
         recipes = cursor.fetchall()
-
         conn.close()
-
-        patiekalai = tk.Toplevel(langas)
-        patiekalai.title("Pasirinkti patiekalai")
-        patiekalai.geometry("800x400")
-
-        reciptu_list = tk.Listbox(patiekalai, width=80, height=10, font=("Arial", 14))
-        reciptu_list.pack()
-
         if not recipes:
-            reciptu_list.destroy()
-            patiekalai.geometry("370x60")
-            nera = tk.Label(patiekalai, text="Receptų su tokia ingredientų kombinacija - nėra",
-                            font=("Ariel", 12, "bold"))
-            uzdaryti = tk.Button(patiekalai, text="Uždaryti", command=patiekalai.destroy)
-            nera.pack()
-            uzdaryti.pack()
-
+            reciptu_list.delete(0, tk.END)
+            selected_text.set("Receptų su tokia ingredientų kombinacija dar nėra")
         else:
-            # Update the Listbox with the selected recipes
+            selected_text.set("")
             atnaujinti(recipes)
 
 
 langas = tk.Tk()
 langas.title("Pasirinkite patiekalą")
 
-
 options = duomenys()
 option_vars = [tk.BooleanVar() for _ in options]
 
+# Create a frame to hold the checkboxes
+checkbox_frame = tk.Frame(langas)
+checkbox_frame.grid(row=0, column=0, sticky='w')
+
 checkboxes = []
 for i, option_name in enumerate(options):
-    checkbox = tk.Checkbutton(langas, text=option_name[0], variable=option_vars[i])
-    checkbox.pack(anchor='w')
+    checkbox = tk.Checkbutton(checkbox_frame, text=option_name[0], variable=option_vars[i])
+    checkbox.grid(row=i, column=0, sticky='w')
     checkboxes.append(checkbox)
 
+patvirtinimo_b = tk.Button(langas, text="Pasirinkti", command=parodyti,font=("Ariel", 11))
+patvirtinimo_b.grid(row=1, column=0, sticky='w', padx=30, pady=10)
 
-patvirtinimo_b = tk.Button(langas, text="Pasirinkau!", command=patvirtinti)
-patvirtinimo_b.pack()
-
+# Create the Listbox widget, but initially, keep it empty
+reciptu_list = tk.Listbox(langas, width=60, height=10, font=("Arial", 14))
+reciptu_list.grid(row=0, column=1, rowspan=2, padx=10, pady=10, sticky='nsew', )
 
 selected_text = tk.StringVar()
-selected_label = tk.Label(langas, textvariable=selected_text)
-selected_label.pack()
+selected_label = tk.Label(langas, textvariable=selected_text, font=("Ariel", 16))
+selected_label.grid(row=2, column=0, columnspan=2, pady=10)
 
+# Configure grid weights for proper resizing
+langas.grid_rowconfigure(0, weight=1)
+langas.grid_rowconfigure(1, weight=1)
+langas.grid_columnconfigure(1, weight=1)
 
 langas.mainloop()
